9dbb04b1916f47d5396269023284f7ac4e2c1bf2
wfishburn1 <137707895+wfishburn1@users.noreply.github.com>
2024-08-18 08:19:39 -0400

    Delete app_server directory

--- app_server/routes/travel.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/travel');
-
-/* GET travel page. */
-router.get('/', controller.travel);
-
-module.exports = router;
