7760fc449857de97d7af756d27e01611928bdcce
wfishburn1 <137707895+wfishburn1@users.noreply.github.com>
2024-08-18 00:16:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
